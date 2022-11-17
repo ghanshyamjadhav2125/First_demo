69a68777b45f2a5fcaf9895a53b52a5d39d8000b
ghanshyamjadhav2125 <ghanshyamjadhav3825@gmail.com>
2022-11-17 09:08:05 +0530

    First commit

---
+++ First_demotest/src/Demo_test/test.java
@@ -0,0 +1,12 @@
+package Demo_test;
+
+import java.util.ArrayList;
+
+public class test {
+
+	public static void main(String[] args) {
+		String name="Ghanshyam";
+		System.out.println(name);
+	}
+
+}
